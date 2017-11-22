5cd18725388020ce1c235d136bfe3da9f43a6f49
pesugier <pierre.sugier@gmail.com>
2017-11-22 15:22:13 +0100

    Loading

--- test.R
+++ test.R
@@ -1,1 +1,12 @@
 #test script
+
+#load libraries
+library(caret)
+library(ggplot2)
+library(dplyr)
+library(plotly)
+library(glmnet)
+library(xgboost)
+library(randomforest)
+library(gbm)
+library(data.table)
